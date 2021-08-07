3dbea66ec38caeea00d036f252e0328c3ede7850
Matt <carlotta.matt@gmail.com>
2021-08-07 12:43:47 -0700

    Cleanup postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,4 @@
 module.exports = {
-  // plugins: {
-  //   ...(process.env.NODE_ENV === "production"
-  //     ? {
-  //         "@fullhuman/postcss-purgecss": {
-  //           content: [
-  //             "./node_modules/react-responsive-carousel/**/**/*.{css,scss}",
-  //             // "./node_modules/name_package/**/**/*.{css,scss}"
-  //             "./pages/**/*.js",
-  //             "./Componets/**/*.js",
-  //           ],
-  //           defaultExtractor: (content) =>
-  //             content.match(/[\w-/:]+(?<!:)/g) || [],
-  //         },
-  //       }
-  //     : {}),
-  //   tailwindcss: {},
-  //   autoprefixer: {},
-  // },
   plugins: [
     "tailwindcss",
     [
